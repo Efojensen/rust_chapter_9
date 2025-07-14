d098d7f317f3f5749403463e8a6c4a9c7e890148
EfoJensen <Jensena231@gmail.com>
2025-07-14 01:20:39 +0000

    Implemented the median functionality too
    With some help from co-pilot

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
 fn main() {
-    let vector: Vec<i32> = vec![23, 12, 7, 23, 11, 23, 7, 4, 6, 2, 1, 76, 43, 52, 12, 65];
+    let mut vector: Vec<i32> = vec![23, 12, 7, 23, 11, 23, 7, 4, 6, 2, 1, 76, 43, 52, 12, 65];
 
-    let mean = mean(&vector);
+    let (mean, length) = mean(&vector);
 
     println!("Vector in question: {:?}", vector);
 
@@ -12,9 +12,18 @@ fn main() {
     let (mode, freq) = mode(&vector);
 
     println!("Mode is {} and it occurs {} times", mode, freq);
+
+    vector.sort();
+
+    let mid = vector.get((length / 2) as usize);
+
+    println!("Median is {}", match mid {
+        Some(value) => value,
+        None => &-1,
+    })
 }
 
-fn mean(vector: &Vec<i32>) -> i32 {
+fn mean(vector: &Vec<i32>) -> (i32, i32) {
     let mut sum = 0;
     let mut length = 0;
 
@@ -23,7 +32,7 @@ fn mean(vector: &Vec<i32>) -> i32 {
         length += 1
     }
 
-    sum / length
+    (sum / length, length)
 }
 
 fn mode(vector: &Vec<i32>) -> (i32, i32) {
